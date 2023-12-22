8d77bfe7d0f7d8dedd586c18e2569145196da0ac
liuqi <469227928@qq.com>
2023-12-22 16:59:28 +0800

    add script to archive history files

---
+++ cms/utils/archive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p cms/data/archives
+mv cms/data/json cms/data/archives
+mv cms/data/repo cms/data/archives
+mkdir -p cms/data/json
+mkdir -p cms/data/repo/github
+mkdir -p cms/data/repo/other
